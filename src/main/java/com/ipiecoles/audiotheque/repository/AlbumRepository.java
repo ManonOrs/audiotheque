53b61e85d44b531dcf4ccdb241732e598acfd527
Manon <manon.orsini5@gmail.com>
2022-03-10 00:10:09 +0100

    fin tp

--- src/main/java/com/ipiecoles/audiotheque/repository/AlbumRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ipiecoles.audiotheque.repository;
-
-import com.ipiecoles.audiotheque.entity.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlbumRepository extends JpaRepository<Artist, Integer> {
-}
